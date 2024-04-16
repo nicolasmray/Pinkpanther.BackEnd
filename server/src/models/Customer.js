1a92796eb44297ee5b6d80b10504f0fe61cf3082
nicolas.mray <nicolas.mray@gmail.com>
2024-04-16 13:09:57 -0300

    models db

---
+++ server/src/models/Customer.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('customer', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM('DEV','ADMIN','GUEST', 'CUSTOMER'),
+      allowNull: false,
+    },
+
+  });
+};
