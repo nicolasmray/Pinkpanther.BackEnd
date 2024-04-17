1d1e1c91314fcf652a96910dee1dfe8c319e7f88
Tobias Dutra <tobias.dutra2009@gmail.com>
2024-04-17 19:16:13 -0300

    sync de db y corregido findAll

--- server/src/controllers/getProduct.js
+++ server/src/controllers/getProduct.js
@@ -2,17 +2,15 @@ const { Variant, Product } = require("../db");
 
 const getProduct = async (req, res) => {
   try {
-    const customer = await Product
-      .findAll
-      //   include: {
-      //     model: Variant,
-      //     attributes: ["id", "name"],
-      //     through: {
-      //       attributes: [],
-      //     },
-      //   },
-      ();
-    console.log(customer);
+    const customer = await Product.findAll({
+      include: {
+        model: Variant,
+        attributes: ["id", "name"],
+        through: {
+          attributes: [],
+        },
+      },
+    });
     return req.status(200).json(customer);
   } catch (error) {
     return res.status(500).json({ error: error.message });
