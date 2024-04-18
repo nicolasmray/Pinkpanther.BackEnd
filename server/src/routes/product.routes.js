a0b474bb70e78470ae24bf4066980c41522f6248
Tobias Dutra <tobias.dutra2009@gmail.com>
2024-04-18 08:55:37 -0300

    postProduct, rutaProduct

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -1,12 +1,9 @@
 const { Router } = require("express");
 const getProduct = require("../controllers/getProduct");
-const postProduct = require("../controllers/postProduct");
-
 
 const product = Router();
 
 product.get("/", getProduct);
-product.post("/" ,postProduct)
 
 // product.delete();
 // product.post();
