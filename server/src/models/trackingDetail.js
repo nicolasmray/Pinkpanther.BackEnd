5b06d80e36236e465b623d9bce20058803ed690c
Tobias Dutra <109052055+TobiasDutra@users.noreply.github.com>
2024-04-18 17:19:27 -0300

    Modelos order, carpetas order (#37)
    * models-Order
    
    Modelos paymentDetail, recepit, tracking
    
    * index,basura
    
    * carpetas,archivos order

--- server/src/models/trackingDetail.js
+++ server/src/models/trackingDetail.js
@@ -3,19 +3,11 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("order", {
+  sequelize.define("trackingDetail", {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
-    },
-    orderDate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    status: {
-      type: DataTypes.STRING,
-      allowNull: false,
     },
     trackingNumber: {
       type: DataTypes.INTEGER,
