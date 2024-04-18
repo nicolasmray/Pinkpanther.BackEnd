b08fd6ec22da9e16835c78b66d63b47f571f419d
Tobias Dutra <tobias.dutra2009@gmail.com>
2024-04-18 16:47:40 -0300

    index,basura

--- server/src/models/Order.js
+++ server/src/models/Order.js
@@ -1,9 +1,9 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('order', {
+  sequelize.define("order", {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
@@ -17,29 +17,13 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    paymentNumber:{
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    paymentDate:{
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    paymentAmount:{
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    receiptAmount:{
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    trackingNumber:{
+    trackingNumber: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
     trackingCourierName: {
       type: DataTypes.INTEGER,
       allowNull: false,
-    }
+    },
   });
 };
