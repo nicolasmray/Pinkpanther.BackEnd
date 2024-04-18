211ff5dabe813cbcdc3a81183fdeb2e2e3e5ce50
Tobias Dutra <tobias.dutra2009@gmail.com>
2024-04-18 11:24:12 -0300

    index, controller en put

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -2,13 +2,13 @@ const { Router } = require("express");
 const getProduct = require("../controllers/products/getProduct");
 const postProduct = require("../controllers/products/postProduct");
 const deleteProduct = require("../controllers/products/deleteProduct");
+const putProduct = require("../controllers/products/putProduct");
 
 const product = Router();
 
 product.get("/", getProduct);
 product.post("/", postProduct);
 product.delete("/:id", deleteProduct);
-// product.post();
-// product.put();
+product.put("/:id", putProduct);
 
 module.exports = product;
