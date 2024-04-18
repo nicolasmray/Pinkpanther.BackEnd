e9d2b2f0b8e6ed5918621df69bcadcac584d50cb
Tobias Dutra <tobias.dutra2009@gmail.com>
2024-04-18 10:36:00 -0300

    Update deleteProduct.js
    borrado codigo basura

--- server/src/controllers/deleteProduct.js
+++ server/src/controllers/deleteProduct.js
@@ -1,21 +1,5 @@
 const { Product } = require("../db");
 
-router.delete("/:id", async (req, res) => {
-  try {
-    // Validar que el ID sea válido
-
-    // Eliminar el producto
-    const deleteProductResult = await deleteProduct(id);
-
-    // Verificar si se eliminó el producto correctamente
-  } catch (error) {}
-});
-
-//eliminar producto
-const deleteProduct = async (id) => {
-  //busco el producto en la base de datos
-};
-
 const deleteProducto = async (req, res) => {
   try {
     const { id } = req.params;
