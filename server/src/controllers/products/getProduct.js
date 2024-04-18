30211ec136571ff4eb06eba0e6fb244a38c0a5d1
Florencia Accastelli <f.accastelli@gmail.com>
2024-04-18 17:28:00 -0300

    getProduct working

--- server/src/controllers/products/getProduct.js
+++ server/src/controllers/products/getProduct.js
@@ -1,17 +1,9 @@
-const { Variant, Product } = require("../../db");
+const { Product } = require("../../db");
 
 const getProduct = async (req, res) => {
   try {
-    const customer = await Product.findAll({
-      include: {
-        model: Variant,
-        attributes: ["id"],
-        through: {
-          attributes: [],
-        },
-      },
-    });
-    return res.status(200).json(customer);
+    const products = await Product.findAll();
+    return res.status(200).json(products);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
