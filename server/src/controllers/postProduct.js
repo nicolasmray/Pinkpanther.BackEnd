a0c1f7c86b5b47fa1f250e019003e434ec107895
Tobias Dutra <tobias.dutra2009@gmail.com>
2024-04-18 10:16:52 -0300

    prettier

--- server/src/controllers/postProduct.js
+++ server/src/controllers/postProduct.js
@@ -1,38 +1,44 @@
-const {Product,Variant} = require("../db");
+const { Product, Variant } = require("../db");
 
-const postProduct = async (req,res) => {
+const postProduct = async (req, res) => {
+  try {
+    const { name, enable, variants } = req.body;
+    console.log("recibo:", name);
 
-try{
- const {name, enable, variants} = req.body
- console.log('recibo:',name)
+    const product = await Product.create({
+      name,
+      enable,
+    });
 
- const product = await Product.create({
-    name,enable
- })
-
- if (variants && variants.length > 0) {
-    for (const variantData of variants) {
-        const { color, priceEfectivo, priceCuotas, size, quantity, photo, supplier } = variantData;
+    if (variants && variants.length > 0) {
+      for (const variantData of variants) {
+        const {
+          color,
+          priceEfectivo,
+          priceCuotas,
+          size,
+          quantity,
+          photo,
+          supplier,
+        } = variantData;
         await Variant.create({
-            color,
-            priceEfectivo,
-            priceCuotas,
-            size,
-            quantity,
-            photo,
-            supplier
-        }).then(variant => {
-    // para asociar la variante creada al producto
-            product.addVariant(variant);
+          color,
+          priceEfectivo,
+          priceCuotas,
+          size,
+          quantity,
+          photo,
+          supplier,
+        }).then((variant) => {
+          // para asociar la variante creada al producto
+          product.addVariant(variant);
         });
+      }
     }
-}
-   return res.status(200).json({ message: 'Se creo con exito el producto' });
- } catch(error){
-    
+    return res.status(200).json({ message: "Se creo con exito el producto" });
+  } catch (error) {
     return res.status(500).json({ error: error.message });
- }
+  }
+};
 
-}
-
-module.exports = postProduct
+module.exports = postProduct;
--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -2,11 +2,10 @@ const { Router } = require("express");
 const getProduct = require("../controllers/getProduct");
 const postProduct = require("../controllers/postProduct");
 
-
 const product = Router();
 
 product.get("/", getProduct);
-product.post("/",postProduct)
+product.post("/", postProduct);
 // product.delete();
 // product.post();
 // product.put();
