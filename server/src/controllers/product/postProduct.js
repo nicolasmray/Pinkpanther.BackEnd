74e81f286860f88d119b02e04bc1a25878f40af2
Tobias Dutra <109052055+TobiasDutra@users.noreply.github.com>
2024-04-18 08:39:48 -0300

    Update postProduct.js
    borrado para que no colisee

--- server/src/controllers/product/postProduct.js
+++ server/src/controllers/product/postProduct.js
@@ -1,29 +1,3 @@
-const hardcodedProduct = {
-  products: [
-    {
-      name: "Top deportivo",
-      photo: {
-        url: "https://drive.google.com/drive/folders/1PGN8jdjpsda5mMIZ9JAjlPLerRTvuvXE",
-      },
 
-      size: "5",
-      color: "verde esmeralda",
-      priceEfectivo: 10800,
-      priceCuotas: 14050,
-      enable: true,
-      quantity: "1",
-    },
-  ],
-};
-
-const postProduct = async (req, res) => {
-  try {
-    return res.status(200).json(hardcodedProduct);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
-};
-
-// revisar codigo para usar con get
 
 module.exports = postProduct;
