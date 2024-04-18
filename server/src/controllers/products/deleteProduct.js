68d0adb6823dd81cf60a7a6abe59a772770aac58
Tobias Dutra <109052055+TobiasDutra@users.noreply.github.com>
2024-04-18 12:33:52 -0300

    Router to routes, cambio delete de issa, package.json delete (#34)

---
+++ server/src/controllers/products/deleteProduct.js
@@ -0,0 +1,38 @@
+const { Product } = require("../../db");
+
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ error: "El ID del producto no es válido" });
+    }
+
+    const findProductToDelete = await Product.findOne({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!findProductToDelete) {
+      throw new Error("No se encontro el producto");
+    } else {
+      await findProductToDelete.destroy();
+    } // si lo encuentra lo destruye
+
+    if (findProductToDelete) {
+      return res.status(200).json({
+        message: `El producto con ID ${id} ha sido eliminado exitosamente`,
+      });
+    } else {
+      return res
+        .status(404)
+        .json({ error: `No se encontró el producto con ID ${id}` });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Ocurrió un error al procesar la solicitud" });
+  }
+};
+
+module.exports = deleteProduct;
--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -1,14 +1,14 @@
 const { Router } = require("express");
-const getProduct = require("../controllers/getProduct");
-const postProduct = require("../controllers/postProduct");
-
+const getProduct = require("../controllers/products/getProduct");
+const postProduct = require("../controllers/products/postProduct");
+const deleteProduct = require("../controllers/products/deleteProduct");
+const putProduct = require("../controllers/products/putProduct");
 
 const product = Router();
 
 product.get("/", getProduct);
-product.post("/",postProduct)
-// product.delete();
-// product.post();
-// product.put();
+product.post("/", postProduct);
+product.delete("/:id", deleteProduct);
+product.put("/:id", putProduct);
 
 module.exports = product;
