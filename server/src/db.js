9ed860ce9b7265a58293a0813538b39ec622bd89
Issa Jaramillo <issakaterinejaramilloalvarez@gmail.com>
2024-04-19 10:00:57 -0500

    category

--- server/src/db.js
+++ server/src/db.js
@@ -26,7 +26,7 @@ fs.readdirSync(path.join(__dirname, "/models"))
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach((model) => model(sequelize));
+ modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [
@@ -54,11 +54,11 @@ Product.belongsToMany(Variant, { through: "VariantProduct" });
 
 Variant.belongsToMany(Category, { through: "VariantCategory" });
 Category.belongsToMany(Variant, { through: "VariantCategory" });
-// Product.hasMany(Reviews);
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
 
-//Probando
+
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,19 +1,22 @@
 const { Router } = require("express");
 const product = require("./product.routes");
+const categoryRouter = require('./category.routes');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-const productRouter = require("./productRouter/product")
+const productRouter = require("./product.routes")
+
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use('/product', productRouter)
+//router.use('/product', productRouter)
 
-
+router.use('/categories', categoryRouter);
 
 router.use("/product", product);
+
 
 module.exports = router;
