cdf04e158c4604924c8b275c6881bf6d9bae2abe
FlorAccastelli <142617063+FlorAccastelli@users.noreply.github.com>
2024-04-19 09:11:53 -0300

    Delete product (#41)
    * productJSON and variant delete
    
    * postProduct working
    
    * getProduct working
    
    * getProductById working
    
    * deleteProduct working
    
    ---------
    
    Co-authored-by: nicolasmray <140557114+nicolasmray@users.noreply.github.com>

--- server/src/db.js
+++ server/src/db.js
@@ -38,7 +38,6 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 
-
 const { Customer, Cart, Product, Order, Category } = sequelize.models;
 
 
