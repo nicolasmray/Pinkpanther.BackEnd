19f76d2fbd80df75117ef04fdb21855322a9195d
nicolas.mray <nicolas.mray@gmail.com>
2024-04-23 09:55:03 -0300

    portcorrection

--- server/index.js
+++ server/index.js
@@ -19,10 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+const { PORT } = process.env;
+port = PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
-    console.log("% listening at 3001"); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log("% listening at " + port); // eslint-disable-line no-console
   });
 });
--- server/src/models/Customer.js
+++ server/src/models/Customer.js
@@ -5,8 +5,8 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('customer', {
     id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
+      type: DataTypes.INTEGER,
+      //defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
     },
     enable: {
