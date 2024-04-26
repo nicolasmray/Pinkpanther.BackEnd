e10bff6597307556a2f8dbf35f977f8fa6f6a002
nicolasmray <140557114+nicolasmray@users.noreply.github.com>
2024-04-26 10:00:06 -0300

    getcustomerid (#73)
    Co-authored-by: nicolas.mray <nicolas.mray@gmail.com>

---
+++ server/src/controllers/customer/getCustomerId.js
@@ -0,0 +1,48 @@
+const { Customer, Cart, Product } = require("../../db.js");
+
+const getCustomerId = async (req, res) => {
+  try {
+    const custId = req.params.id
+    console.log(custId)
+    const  customer  = await Customer.findOne({
+        where: {
+            id: custId
+        },
+        include: [{
+            model: Cart,
+            include: [Product]
+          }]
+    });
+    
+    if (customer) {
+        return res.status(200).json({ 
+            message: "Se encontró el customer:", 
+            customer: {
+            id: customer.id,
+            enable: customer.enable,
+            userName: customer.userName,
+            password: customer.password,
+            role: customer.role,
+            DNI: customer.DNI,
+            birthdate: customer.birthdate,
+            firstName: customer.firstName,
+            lastName: customer.lastName,
+            email: customer.email,
+            telephone: customer.telephone,
+            country: customer.country,
+            city: customer.city,
+            street: customer.street,
+            streetNumber: customer.streetNumber,
+            apartmentNumber: customer.apartmentNumber,
+            postalCode: customer.postalCode
+            }
+        });
+    }
+    return res.status(400).json({ message: "No se encontró el customer:" })
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = getCustomerId;
