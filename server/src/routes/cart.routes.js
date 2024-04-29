b187f1ed2045010713bd9eb905e8f4d0b399996b
Celeste Pucheta <puchetaceleste18@gmail.com>
2024-04-29 08:39:15 -0300

    actulizandoRutas

--- server/src/routes/cart.routes.js
+++ server/src/routes/cart.routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
-const  {getCartProducts, createCart, updateCartItem, addToCart, deleteProductsFromCart}  = require("../controllers/cart/cart");
+const { getCartProducts, getAllCartProducts,createCart, updateCartItem, addToCart, deleteCart, deleteProductFromCart, deleteAllProductsFromCart } = require("../controllers/cart/cart");
 
 const cart = Router();
 
 cart.get('/:id', getCartProducts);
-cart.post('/add', addToCart); 
+cart.get('/',getAllCartProducts)
 cart.post('/create', createCart);
 cart.put('/update', updateCartItem);
-cart.delete('/delete/:idCustomer/:productId?', deleteProductsFromCart);
+cart.delete('/delete/:cartId', deleteCart);
+cart.delete('/:cartId/products/:productId', deleteProductFromCart);
+cart.delete('/:id/products', deleteAllProductsFromCart);
 
 module.exports = cart;
