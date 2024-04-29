cfc48325567bbf90eaab7a2bc25f51a694ac31cc
nicolas.mray <nicolas.mray@gmail.com>
2024-04-29 20:59:05 -0300

    corsorigin solution

--- server/src/app.js
+++ server/src/app.js
@@ -16,13 +16,12 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  const allowedOrigins = ['http://localhost:5173', 'https://pinkpanther-backend-ip0f.onrender.com', 'http://localhost:3001', ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
