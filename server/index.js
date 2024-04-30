868a74244412752b2ed7725c6d16d4a12a725d8d
Florencia Accastelli <f.accastelli@gmail.com>
2024-04-30 19:21:53 -0300

    adding-create-product

--- server/index.js
+++ server/index.js
@@ -26,9 +26,9 @@ const port = PORT
 
 
 // Syncing all the models at once.
-//conn.sync({ alter: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(port, () => {
     console.log("% listening at " + port); // eslint-disable-line no-console
   });
- //}); 
+ }); 
 
