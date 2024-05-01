c0964714f8cc47eaea5bfcabff9d25b715cbfa3b
FlorAccastelli <142617063+FlorAccastelli@users.noreply.github.com>
2024-05-01 19:38:55 -0300

    script-create-products (#100)

--- server/index.js
+++ server/index.js
@@ -20,8 +20,8 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { PORT } = process.env;
-//const port = 3001;
-const port = PORT
+const port = 3001;
+/* const port = PORT */
 
 
 // Syncing all the models at once.
